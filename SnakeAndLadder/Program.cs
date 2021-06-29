ae218e516c7d5dcc914e897a2e7203bb3ab20bfd
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-06-29 18:36:33 +0530

    [Harshitha] Assigning initial position of the player

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        public const int START_POSITION = 0;
+        public const int FINAL_POSITION = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Game");
+            int playerPosition = 0;
+            Console.WriteLine("Player Initial Position : "+playerPosition);
+
+        }
+    }
+}
