ad69352336c0e3df0503e8b3680adf8ede7fc6a5
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-06-29 18:57:22 +0530

    [Harshitha] Using Random function to get player rolldice

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,7 +4,9 @@ namespace SnakeAndLadder
 {
     class Program
     {
+        //Sarting Position is 0
         public const int START_POSITION = 0;
+        //Final Position is 100 and if the player reach 100 he wins the game
         public const int FINAL_POSITION = 100;
         static void Main(string[] args)
         {
@@ -12,6 +14,10 @@ namespace SnakeAndLadder
             //Initial Position of the Player
             int playerPosition = 0;
             Console.WriteLine("Player Initial Position : "+playerPosition);
+            Random random = new Random();
+            //Player rolls the dice and gets value of the dice
+            int playerRollDice = random.Next(1, 7);
+            Console.WriteLine("Player Dice Value is : ",+playerRollDice);
 
         }
     }
