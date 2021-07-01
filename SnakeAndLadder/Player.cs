413256f7e3a1278da84bdbab515ad73533b7c297
SolletiHarshitharigin <solletiharshitha@gmail.com>
2021-07-01 07:53:10 +0530

    [Harshitha] Multi player game

---
+++ SnakeAndLadder/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    class Player
+    {
+        public int playerPosition, playerNextMove, countOfDiceRolled;
+        public string playerName;
+        public Player(string Name)
+        {
+            this.playerName = Name;
+            this.playerPosition = 0;
+            this.playerNextMove = 0;
+            this.countOfDiceRolled = 0;
+        }
+    }
+}
--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -13,11 +13,18 @@ namespace SnakeAndLadder
         public const int NO_PLAY = 0;
         public const int LADDER = 1;
         public const int SNAKE = 2;
+
+        //public static int Roll_Dice()
+        //{
+        // random value is given to palyer's dice
+        //return random.Next(1, 7);
+        // }
         public static int PlayerMoves(int playerRollDice)
         {
             int playerMove = 0;
-            int option = random.Next(0,3);
-            switch(option)
+            // Player Option is gentrated..
+            int option = random.Next(0, 3);
+            switch (option)
             {
                 case LADDER:
                     playerMove = playerRollDice;
@@ -27,40 +34,68 @@ namespace SnakeAndLadder
                     break;
                 default:
                     break;
-
             }
             return playerMove;
         }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake And Ladder Game");
-            //Initial Position of the Player
-            int playerPosition = 0,countOfDiceRolling=0;
-            Console.WriteLine("Player Initial Position : "+playerPosition);
-            while(playerPosition<FINAL_POSITION)
+            Console.WriteLine("Wellcome To Snake and Ladder game");
+            // Number of players are created 
+            Player player1 = new Player("Player 1");
+            Player player2 = new Player("Player 2");
+            bool endGame = false;
+            Player currentPlayer = player1;
+
+
+            //Repeats till the Player reaches the winning position 100.
+            while (!endGame)
             {
-                //Player rolls the dice and gets value of the dice
-                int playerRollDice = random.Next(1, 7);
-                countOfDiceRolling++;
-                int playerCurrentMove = PlayerMoves(playerRollDice);
-                int playerNextMove = playerPosition + playerCurrentMove;
-                if (playerNextMove < START_POSITION)
+                // player rolls the dice and gets the value
+                int player_Roll_Dice = random.Next(1, 7);
+
+                // number of times the dice was played is counted
+                currentPlayer.countOfDiceRolled++;
+
+                // The Player then checks for a Option. They are No_Play,Ladder and Snake.
+                int player_move = PlayerMoves(player_Roll_Dice);
+                if (currentPlayer.playerPosition + player_move == FINAL_POSITION)
                 {
-                    playerPosition = START_POSITION;
+                    currentPlayer.playerNextMove = FINAL_POSITION;
+                    Console.WriteLine(currentPlayer.playerName + " current position after rolling the Dice is " + currentPlayer.playerNextMove);
+                    Console.WriteLine();
+                    Console.WriteLine("The Winner is " + currentPlayer.playerName);
+                    Console.WriteLine("Total Number of times Dice Rolled in the game by " + currentPlayer.playerName + " is " + currentPlayer.countOfDiceRolled);
+                    System.Environment.Exit(0);
                 }
-                else if(playerNextMove>FINAL_POSITION)
+                else if (currentPlayer.playerPosition + player_move > FINAL_POSITION)
+                    currentPlayer.playerNextMove = currentPlayer.playerPosition;
+                else
+                    currentPlayer.playerNextMove = currentPlayer.playerPosition + player_move;
+
+                if (currentPlayer.playerNextMove < START_POSITION)
+                    currentPlayer.playerPosition = START_POSITION;
+                else
+                    currentPlayer.playerPosition = currentPlayer.playerNextMove;
+
+                // Player current position After rolling the Dice
+                Console.WriteLine(currentPlayer.playerName + " current position after rolling the Dice is " + currentPlayer.playerPosition);
+                if (player_move == 6)
                 {
-                    playerPosition = playerNextMove - playerCurrentMove;
+                    continue;
                 }
                 else
                 {
-                    playerPosition = playerNextMove;
+                    if (currentPlayer == player1)
+                        currentPlayer = player2;
+                    else
+                        currentPlayer = player1;
                 }
-                Console.WriteLine("Player current position after rolling the dice is : "+playerPosition);
             }
-            Console.WriteLine();
-            Console.WriteLine("Player Current Position after the game is: " + playerPosition);
-            Console.WriteLine("Total number of times the dice rolled in the game is : "+countOfDiceRolling);
+
         }
+
     }
 }
+
+
+
